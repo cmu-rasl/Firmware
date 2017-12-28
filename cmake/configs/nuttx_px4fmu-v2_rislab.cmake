593959d0515871a4fa80269f0b56de0dfb6f5b29
Alex Spitzer <aes368@cornell.edu>
2017-12-28 10:56:03 +0000

    Update custom CMU cmake configs to conform to defaults

--- cmake/configs/nuttx_px4fmu-v2_rislab.cmake
+++ cmake/configs/nuttx_px4fmu-v2_rislab.cmake
@@ -1,10 +1,5 @@
 include(nuttx/px4_impl_nuttx)
-
-px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common)
-
-set(CMAKE_TOOLCHAIN_FILE ${PX4_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-none-eabi.cmake)
-
-set(config_uavcan_num_ifaces 2)
+px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common IO px4io-v2)
 
 set(config_module_list
 	#
@@ -94,22 +89,3 @@ set(config_module_list
 	platforms/nuttx
 	platforms/nuttx/px4_layer
 )
-
-set(config_extra_builtin_cmds
-	serdis
-	sercon
-	)
-
-set(config_io_board
-	px4io-v2
-	)
-
-add_custom_target(sercon)
-set_target_properties(sercon PROPERTIES
-	PRIORITY "SCHED_PRIORITY_DEFAULT"
-	MAIN "sercon" STACK_MAIN "2048")
-
-add_custom_target(serdis)
-set_target_properties(serdis PROPERTIES
-	PRIORITY "SCHED_PRIORITY_DEFAULT"
-	MAIN "serdis" STACK_MAIN "2048")
